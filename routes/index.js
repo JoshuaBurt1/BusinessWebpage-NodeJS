c0cd8f7864d81ef65a7b1b7c9be8b20611298480
Joshua Burt <Joshua.Burt@MyGeorgian.ca>
2025-01-16 20:46:55 -0500

    Owner access only for CRUD database, registration removed

--- routes/index.js
+++ routes/index.js
@@ -20,53 +20,12 @@ router.post(
   })
 );
 
-// GET handler for /register (user loads page)
-router.get("/register", (req, res, next) => {
-  res.render("register", { title: "Create an Account" });
-});
-
-//POST handler for /register
-router.post("/register", (req, res, next) => {
-  // Create a new user based on the information from the page
-  User.register(
-    new User({
-      username: req.body.username,
-    }),
-    req.body.password,
-    (err, newUser) => {
-      if (err) {
-        console.log(err);
-        // take user back and reload register page
-        return res.redirect("/register");
-      } else {
-        // log user in
-        req.login(newUser, (err) => {
-          res.redirect("/projects");
-        });
-      }
-    }
-  );
-});
-
 // GET handler for logout
 router.get("/logout", (req, res, next) => {
   req.logout(function (err) {
     res.redirect("/login");
   });
 });
-
-// GET handler for /github
-// call passport authenticate and pass the name of the strategy 
-// and the information we require from github
-router.get('/github', passport.authenticate('github', { scope: ['user.email'] }));
-// GET handler for /github/callback 
-// this is the url they come back to after entering their credentials
-router.get('/github/callback',
-  // callback to send user back to login if unsuccessful
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  // callback when login is successful
-  (req, res, next) => { res.redirect('/projects') }
-);
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
